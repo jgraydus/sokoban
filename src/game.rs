a0fd3622485b1633d89691d29e99af79267535e4
josh <jgraydus@gmail.com>
2025-02-13 16:37:58 -0700

    bitmaps for wall, floors, and boxes

--- src/game.rs
+++ src/game.rs
@@ -1,3 +1,4 @@
+use crate::blocks;
 use crate::constants::*;
 use crate::engine::*;
 use crate::engine::events::*;
@@ -8,223 +9,17 @@ use std::{
     default::Default,
 };
 use wasm_bindgen::prelude::*;
+use web_sys::ImageBitmap;
 
 const CELLS: u32 = 8;
 const CELL_SIZE: f64 = SIZE as f64 / CELLS as f64;
 
-struct GoalSprite {}
-
-impl Sprite for GoalSprite {
-    fn draw(&self, cxt: &web_sys::CanvasRenderingContext2d, loc: Location, size: Size) {
-        cxt.set_fill_style(&JsValue::from_str("#664422"));
-        cxt.begin_path();
-        cxt.ellipse(loc.x * CELL_SIZE + CELL_SIZE / 2.0,
-                    loc.y * CELL_SIZE + CELL_SIZE / 2.0,
-                    CELL_SIZE / 4.0,
-                    CELL_SIZE / 4.0,
-                    0.0,
-                    0.0,
-                    360.0);
-        cxt.fill();
-    }
-}
-
-struct BlockSprite {}
-
-impl Sprite for BlockSprite {
-    fn draw(&self, cxt: &web_sys::CanvasRenderingContext2d, loc: Location, size: Size) {
-        cxt.set_fill_style(&JsValue::from_str("#224466"));
-        cxt.fill_rect(loc.x * CELL_SIZE + 1.0,
-                      loc.y * CELL_SIZE + 1.0,
-                      CELL_SIZE - 2.0,
-                      CELL_SIZE - 2.0);
-    }
-}
-
-struct PlayerSprite {}
-
-impl Sprite for PlayerSprite {
-    fn draw(&self, cxt: &web_sys::CanvasRenderingContext2d, loc: Location, size: Size) {
-        cxt.set_fill_style(&JsValue::from_str("#66FF88"));
-        cxt.begin_path();
-        cxt.ellipse(loc.x * CELL_SIZE + CELL_SIZE / 2.0,
-                    loc.y * CELL_SIZE + CELL_SIZE / 2.0,
-                    CELL_SIZE / 4.0,
-                    CELL_SIZE / 4.0,
-                    0.0,
-                    0.0,
-                    360.0);
-        cxt.fill();
-    }
-}
-
-
-type EntityId = i32;
-
-#[derive(Clone,Debug)]
-enum EntityType { Block, Goal, Player }
-
-#[derive(Default)]
-pub struct Game_ {
-    walls: Vec<Location>,
-    entities: Vec<EntityId>,
-    locations: BTreeMap<EntityId, Location>,
-    entity_types: BTreeMap<EntityId, EntityType>,
-    sprites: BTreeMap<EntityId, Box<dyn Sprite>>,
-    background: Option<OffscreenCanvasSprite>,
-}
-
-impl Game_ {
-    pub fn from_string(s: String) -> Self {
-        let mut g = Game_::default();
-
-        let mut next_id: i32 = 0;
-
-        for (y, line) in s.lines().enumerate() {
-            for (x, c) in line.chars().enumerate() {
-                match c {
-                    'O' => {
-                        g.walls.push(Location { x: x as f64, y: y as f64 });
-                    }
-                    '.' => {
-                        let id = next_id;
-                        next_id = next_id + 1;
-                        g.entities.push(id);
-                        g.entity_types.insert(id, EntityType::Goal);
-                        g.sprites.insert(id, Box::new(GoalSprite {}));
-                        g.locations.insert(id, Location { x: x as f64, y: y as f64 });
-                    }
-                    'x' => {
-                        let id = next_id;
-                        next_id = next_id + 1;
-                        g.entities.push(id);
-                        g.entity_types.insert(id, EntityType::Block);
-                        g.sprites.insert(id, Box::new(BlockSprite {}));
-                        g.locations.insert(id, Location { x: x as f64, y: y as f64 });
-                    }
-                    'X' => {
-                        {
-                            let id = next_id;
-                            next_id = next_id + 1;
-                            g.entities.push(id);
-                            g.entity_types.insert(id, EntityType::Block);
-                            g.sprites.insert(id, Box::new(BlockSprite {}));
-                            g.locations.insert(id, Location { x: x as f64, y: y as f64 });
-                        }
-                        {
-                            let id = next_id;
-                            next_id = next_id + 1;
-                            g.entities.push(id);
-                            g.entity_types.insert(id, EntityType::Goal);
-                            g.sprites.insert(id, Box::new(GoalSprite {}));
-                            g.locations.insert(id, Location { x: x as f64, y: y as f64 });
-                        }
-                    }
-                    'p' => {
-                        let id = next_id;
-                        next_id = next_id + 1;
-                        g.entities.push(id);
-                        g.entity_types.insert(id, EntityType::Player);
-                        g.sprites.insert(id, Box::new(PlayerSprite {}));
-                        g.locations.insert(id, Location { x: x as f64, y: y as f64 });
-                    }
-                    'P' => {
-                        {
-                            let id = next_id;
-                            next_id = next_id + 1;
-                            g.entities.push(id);
-                            g.entity_types.insert(id, EntityType::Player);
-                            g.sprites.insert(id, Box::new(PlayerSprite {}));
-                            g.locations.insert(id, Location { x: x as f64, y: y as f64 });
-                        }
-                        {
-                            let id = next_id;
-                            next_id = next_id + 1;
-                            g.entities.push(id);
-                            g.entity_types.insert(id, EntityType::Goal);
-                            g.sprites.insert(id, Box::new(GoalSprite {}));
-                            g.locations.insert(id, Location { x: x as f64, y: y as f64 });
-                        }
-                    }
-                    _ => {}
-                }
-            }
-        }
-
-        g.render_background();
-
-        g
-    }
-
-    fn render_background(&mut self) {
-        let canvas = web_sys::OffscreenCanvas::new(SIZE, SIZE).expect("failed to create offscreen canvas");
-        let cxt = canvas.get_context("2d")
-            .expect("failed to get Context2d")
-            .expect("Context2d missing")
-            .dyn_into::<web_sys::OffscreenCanvasRenderingContext2d>()
-            .expect("failed to convert result into Context2d");
-
-        cxt.set_fill_style(&JsValue::from_str("#444444"));
-        cxt.fill_rect(0.0, 0.0, SIZE as f64, SIZE as f64);
-
-        cxt.set_stroke_style(&JsValue::from_str("#333333"));
-        for row in 0..CELLS {
-            for col in 0..CELLS {
-                let x = col as f64 * CELL_SIZE;
-                let y = row as f64 * CELL_SIZE;
-                cxt.stroke_rect(x, y, CELL_SIZE, CELL_SIZE);
-            }
-        }
-
-        // draw walls
-        cxt.set_fill_style(&JsValue::from_str("#111111"));
-        for item in &self.walls {
-            cxt.fill_rect(item.x * CELL_SIZE + 1.0,
-                          item.y * CELL_SIZE + 1.0,
-                          CELL_SIZE - 2.0,
-                          CELL_SIZE - 2.0);
-        }
-
-        self.background = Some(OffscreenCanvasSprite::new(canvas))
-    }
-
-    pub fn draw(&self, cxt: &web_sys::CanvasRenderingContext2d) {
-        // draw background
-        self.background
-            .as_ref()
-            .unwrap()
-            .draw(&cxt,
-                  Location { x: 0.0, y: 0.0 },
-                  Size { w: SIZE as f64, h: SIZE as f64 });
-
-
-        // draw blocks
-        for (id, spr) in self.sprites.iter() {
-            if let Some(EntityType::Block) = self.entity_types.get(&id) {
-                if let Some(loc) = self.locations.get(&id) {
-                    spr.draw(cxt, loc.clone(), Size { w: CELL_SIZE, h: CELL_SIZE });
-                }
-            }
-        }
-
-        // draw goals
-        for (id, spr) in self.sprites.iter() {
-            if let Some(EntityType::Goal) = self.entity_types.get(&id) {
-                if let Some(loc) = self.locations.get(&id) {
-                    spr.draw(cxt, loc.clone(), Size { w: CELL_SIZE, h: CELL_SIZE });
-                }
-            }
-        }
-
-        // draw player
-        for (id, spr) in self.sprites.iter() {
-            if let Some(EntityType::Player) = self.entity_types.get(&id) {
-                if let Some(loc) = self.locations.get(&id) {
-                    spr.draw(cxt, loc.clone(), Size { w: CELL_SIZE, h: CELL_SIZE });
-                }
-            }
-        }
-    }
+#[derive(PartialOrd, Ord, PartialEq, Eq, Debug)]
+pub enum SpriteType {
+    Player,
+    Wall,
+    Floor,
+    Moveable,
 }
 
 pub struct Game {
@@ -233,16 +28,18 @@ pub struct Game {
     goals: Vec<Location>,
     pub player: Location,
     background: Option<OffscreenCanvasSprite>,
+    sprites: BTreeMap<SpriteType, ImageBitmap>, 
 }
 
 impl Game {
-    pub fn from_string(s: String) -> Self {
+    pub fn from_string(sprites: BTreeMap<SpriteType, ImageBitmap>, s: String) -> Self {
         let mut g = Self {
             walls: Vec::new(),
             blocks: Vec::new(),
             goals: Vec::new(),
             player: Location { x: 0.0, y: 0.0 },
             background: None,
+            sprites,
         };
 
         for (y, line) in s.lines().enumerate() {
@@ -265,8 +62,8 @@ impl Game {
         g
     }
 
-    pub fn example() -> Game {
-        Game::from_string(
+    pub fn example(sprites: BTreeMap<SpriteType, ImageBitmap>) -> Game {
+        Game::from_string(sprites,
 r"  OOOOO
 OOO   O
 O.px  O
@@ -332,12 +129,14 @@ OOOOOOOO".into())
                   Size { w: SIZE as f64, h: SIZE as f64 });
 
         // draw blocks
-        cxt.set_fill_style(&JsValue::from_str("#224466"));
+        cxt.set_fill_style(&JsValue::from_str("#660000"));
         for item in &self.blocks {
-            cxt.fill_rect(item.x * CELL_SIZE + 1.0,
-                          item.y * CELL_SIZE + 1.0,
-                          CELL_SIZE - 2.0,
-                          CELL_SIZE - 2.0);
+            cxt.draw_image_with_image_bitmap_and_dw_and_dh(
+                &self.sprites.get(&SpriteType::Moveable).unwrap(),
+                item.x * CELL_SIZE,
+                item.y * CELL_SIZE,
+                CELL_SIZE,
+                CELL_SIZE);
         }
 
         // draw goals
@@ -384,6 +183,7 @@ OOOOOOOO".into())
             .expect("Context2d missing")
             .dyn_into::<web_sys::OffscreenCanvasRenderingContext2d>()
             .expect("failed to convert result into Context2d");
+        cxt.set_image_smoothing_enabled(false);
 
         cxt.set_fill_style(&JsValue::from_str("#444444"));
         cxt.fill_rect(0.0, 0.0, SIZE as f64, SIZE as f64);
@@ -393,17 +193,19 @@ OOOOOOOO".into())
             for col in 0..CELLS {
                 let x = col as f64 * CELL_SIZE;
                 let y = row as f64 * CELL_SIZE;
-                cxt.stroke_rect(x, y, CELL_SIZE, CELL_SIZE);
+                cxt.draw_image_with_image_bitmap_and_dw_and_dh(
+                    &self.sprites.get(&SpriteType::Floor).unwrap(), x, y, CELL_SIZE, CELL_SIZE);
             }
         }
 
         // draw walls
-        cxt.set_fill_style(&JsValue::from_str("#111111"));
         for item in &self.walls {
-            cxt.fill_rect(item.x * CELL_SIZE + 1.0,
-                          item.y * CELL_SIZE + 1.0,
-                          CELL_SIZE - 2.0,
-                          CELL_SIZE - 2.0);
+            cxt.draw_image_with_image_bitmap_and_dw_and_dh(
+                &self.sprites.get(&SpriteType::Wall).unwrap(),
+                item.x * CELL_SIZE,
+                item.y * CELL_SIZE,
+                CELL_SIZE,
+                CELL_SIZE);
         }
 
         self.background = Some(OffscreenCanvasSprite::new(canvas))
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+mod blocks;
 mod constants;
 mod engine;
 mod game;
@@ -10,16 +11,66 @@ use crate::engine::*;
 use crate::sprite::*;
 use futures::channel::mpsc::channel;
 use futures::stream::StreamExt;
+use std::collections::BTreeMap;
 use wasm_bindgen::prelude::*;
+use wasm_bindgen_futures::JsFuture;
 
 #[wasm_bindgen]
 pub fn run() {
     utils::set_panic_hook();
 
     wasm_bindgen_futures::spawn_local(async move {
+
         let canvas = utils::get_canvas();
         canvas.set_height(SIZE);
         canvas.set_width(SIZE);
-        Engine::start(canvas, Game::example()).await;
+ 
+        let cxt = canvas.get_context("2d")
+            .expect("failed to get Context2d")
+            .expect("Context2d missing")
+            .dyn_into::<web_sys::CanvasRenderingContext2d>()
+            .expect("failed to convert result into Context2d");
+        cxt.set_image_smoothing_enabled(false);
+
+        let wall = {
+            let tmp = web_sys::window()
+                .expect("window not found?!")
+                .create_image_bitmap_with_image_data(&blocks::wall())
+                .unwrap();
+            JsFuture::from(tmp).await
+                .expect("FOOOOOOO!")
+                .dyn_into::<web_sys::ImageBitmap>()
+                .expect("on no")
+        };
+
+        let floor = {
+            let tmp = web_sys::window()
+                .expect("window not found?!")
+                .create_image_bitmap_with_image_data(&blocks::floor())
+                .unwrap();
+            JsFuture::from(tmp).await
+                .expect("FOOOOOOO!")
+                .dyn_into::<web_sys::ImageBitmap>()
+                .expect("on no")
+        };
+
+        let moveable = {
+            let tmp = web_sys::window()
+                .expect("window not found?!")
+                .create_image_bitmap_with_image_data(&blocks::moveable())
+                .unwrap();
+            JsFuture::from(tmp).await
+                .expect("FOOOOOOO!")
+                .dyn_into::<web_sys::ImageBitmap>()
+                .expect("on no")
+        };
+
+        let mut sprites = BTreeMap::from([
+            (SpriteType::Wall, wall),
+            (SpriteType::Floor, floor),
+            (SpriteType::Moveable, moveable),
+        ]);
+
+        Engine::start(canvas, Game::example(sprites)).await;
     });
 }
